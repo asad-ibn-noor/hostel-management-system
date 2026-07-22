b487e10c0f4da34a1c63f78284d5bfaf120f7c3e
Muhammad Asad Khan <asad.ibn.noor@gmail.com>
2026-07-22 16:38:32 +0500

    Updated Authentication logic

--- hms_backend/controllers/AuthController.js
+++ hms_backend/controllers/AuthController.js
@@ -4,10 +4,10 @@ const { User, Student } = require('../models');
 
 async function register(req, res) {
   try {
-    const { email, password, role, full_name, student_id_no, gender, contact, emergency_contact, admission_date } = req.body;
+    const { email, password, role = "student", full_name, student_id_no, gender, contact, emergency_contact, admission_date } = req.body;
 
-    if (!email || !password || !role) {
-      return res.status(400).json({ error: 'email, password, and role are required' });
+    if (!email || !password) {
+      return res.status(400).json({ error: 'email and password are required' });
     }
 
     const existing = await User.findOne({ where: { email } });
